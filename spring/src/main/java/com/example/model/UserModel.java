73889b34ea1bd42d02e3e903fba5154d8a6d5809
GowthamanM-Neo <gowthaman@iamneo.ai>
2021-03-17 15:08:13 +0530

    User Model Defined

---
+++ spring/src/main/java/com/example/model/UserModel.java
@@ -0,0 +1,51 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserModel {
+
+	@Id
+	String email;
+	String password;
+	String mobileNumber;
+	String username;
+	int age;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "UserModel [email=" + email + ", password=" + password + ", mobileNumber=" + mobileNumber + ", username="
+				+ username + ", age=" + age + "]";
+	}
+	
+}
